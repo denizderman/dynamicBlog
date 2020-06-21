62a311328cf14b51239218e29730db4c1e09bcce
denizderman <denzderman@gmail.com>
2020-06-21 22:04:03 +0100

    add content inside home, header and footer page

--- app.js
+++ app.js
@@ -11,8 +11,15 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+
+const homePageContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis fermentum tortor nec venenatis. Mauris ac arcu sagittis, bibendum sapien id, tincidunt sapien. Aliquam eget scelerisque urna. Nunc finibus velit iaculis lacus luctus tempor. Phasellus tristique pharetra auctor. Pellentesque blandit, justo at consequat eleifend, nisi ipsum consequat est, id ultrices mauris ligula luctus augue. Nunc cursus pretium enim, scelerisque tristique enim. Curabitur suscipit, turpis et posuere cursus, nulla nunc dapibus dolor, in finibus ex velit vitae sem."
+
+
 app.get("/", function(req, res) {
-    res.render("home");
+
+    res.render("home", {pageContent: homePageContent});
+
+    
 });
 
 app.listen(3000, function() {
