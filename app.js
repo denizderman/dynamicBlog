9ce1c4acc1230ab621183d47a7af2b06db7cd79c
denizderman <denzderman@gmail.com>
2020-06-22 23:21:56 +0100

    create partial folder and add header and footer inside. Organize pages according to the header and footer

--- app.js
+++ app.js
@@ -12,14 +12,33 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 
-const homePageContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis fermentum tortor nec venenatis. Mauris ac arcu sagittis, bibendum sapien id, tincidunt sapien. Aliquam eget scelerisque urna. Nunc finibus velit iaculis lacus luctus tempor. Phasellus tristique pharetra auctor. Pellentesque blandit, justo at consequat eleifend, nisi ipsum consequat est, id ultrices mauris ligula luctus augue. Nunc cursus pretium enim, scelerisque tristique enim. Curabitur suscipit, turpis et posuere cursus, nulla nunc dapibus dolor, in finibus ex velit vitae sem."
+const homePageContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis fermentum tortor nec venenatis. Mauris ac arcu sagittis, bibendum sapien id, tincidunt sapien. Aliquam eget scelerisque urna. Nunc finibus velit iaculis lacus luctus tempor. Phasellus tristique pharetra auctor. Pellentesque blandit, justo at consequat eleifend, nisi ipsum consequat est, id ultrices mauris ligula luctus augue. Nunc cursus pretium enim, scelerisque tristique enim. Curabitur suscipit, turpis et posuere cursus, nulla nunc dapibus dolor, in finibus ex velit vitae sem.";
+
+const aboutPageContent = "Ut ac ornare sapien, vel posuere ipsum. Sed pharetra diam sed eleifend fringilla. Ut laoreet nulla quis mauris rutrum pellentesque. Pellentesque facilisis ligula vel nulla malesuada gravida. In ultrices mauris est, quis rhoncus ligula finibus et. Phasellus sodales, libero ut cursus lobortis, risus urna auctor nibh, sed tempor velit est eu metus. Nulla iaculis tincidunt convallis. Maecenas tincidunt nunc vitae diam elementum ultricies.";
+
+const contactPageContent = "Proin at erat rutrum, fringilla nisi id, placerat lectus. Duis at suscipit quam, in cursus nulla. Maecenas elementum leo ut orci tempor, sed vehicula lectus rhoncus. Vivamus feugiat massa nec lacus sollicitudin, nec blandit nisl vulputate. Fusce quis magna est. Sed ornare rhoncus nisi et dictum.";
+
 
 
 app.get("/", function(req, res) {
 
-    res.render("home", {pageContent: homePageContent});
+    res.render("home", {homePage: homePageContent});
+       
+});
 
-    
+app.get("/about", function(req, res) {
+
+    res.render("about", {aboutPage: aboutPageContent});
+});
+
+app.get("/contact", function(req, res) {
+
+    res.render("contact", {contactPage: contactPageContent});
+});
+
+app.get("/compose", function(req, res) {
+
+    res.render("compose");
 });
 
 app.listen(3000, function() {
